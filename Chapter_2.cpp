c74ff4c539f4064b76c6d5a6e2d75ee62f56c264
arhantbararia <arhantbararia@gmail.com>
2021-09-14 00:41:09 +0530

    2.1 done

--- Chapter_2.cpp
+++ Chapter_2.cpp
@@ -5,6 +5,161 @@
 void twopointone()
 {
     system("CLS");
+    // EXCHANGING VALUES OF VARIABLES
+    int choice;
+    std::cout<<"1. PROBLEM"<<std::endl;
+    std::cout<<"2. supplement problem 2.1.1"<<std::endl;
+    std::cout<<"3. supplement problem 2.1.2"<<std::endl;
+    std::cout<<"4. supplement problem 2.1.3"<<std::endl;
+    std::cout<<"5. supplement problem 2.1.4"<<std::endl;
+    std::cout<<"6. supplement problem 2.1.5"<<std::endl;
+    std::cout<<"\n Enter your Choice : ";
+    std::cin>>choice;
+    switch(choice)
+    {
+        case 1:
+        {
+            // BASE PROBLEM
+            system("CLS");
+            int a , b;
+            std::cout<<"enter value of A: ";
+            std::cin>>a;
+            std::cout<<"enter value of B: ";
+            std::cin>>b;
+            std::cout<<"Values before the swap \n ";
+            std::cout<<"A = "<<a<<" B = "<<b;
+            
+            int temp;
+            temp =a ;
+            a = b;
+            b = temp;
+
+            std::cout<<"Values after the swap\n";
+            std::cout<<"A = "<<a<<" B = "<<b;
+
+            
+        }
+        break;
+
+        case 2:
+        {   //supplement problem 2.1.1
+            system("CLS");
+            std::cout<<"Bring a third glass";
+
+        }
+        break;
+
+        case 3:
+        {
+            //supplement problem 2.1.2
+            system("CLS");
+            int a, b, c ;
+            std::cout<<"enter value of A: ";
+            std::cin>>a;
+            std::cout<<"enter value of B: ";
+            std::cin>>b;
+            std::cout<<"enter the value of C: ";
+            std::cin>>c;
+        
+            std::cout<<"Values before the swap \n ";
+            std::cout<<"A = "<<a<<" B = "<<b<<" C = "<<c ;
+            
+            int temp;
+            temp = c;
+            c = b;
+            b =a ;
+            a = temp;
+
+            std::cout<<"A = "<<a<<" B = "<<b<<" C = "<<c ;
+
+        }
+        break;
+        case 4:
+        {
+            //supplement problem 2.1.3
+            int a , b ,c , d;
+            std::cout<<"enter value of A: ";
+            std::cin>>a;
+            std::cout<<"enter value of B: ";
+            std::cin>>b;
+            std::cout<<"enter the value of C: ";
+            std::cin>>c;
+            std::cout<<"Enter the value of D: ";
+            std::cin>>d;
+
+            std::cout<<"Values before the swap \n ";
+            std::cout<<"A = "<<a<<" B = "<<b<<" C = "<<c<<" D = "<<d;
+
+            int temp;
+            temp =a ;
+            a = b;
+            b = c ;
+            c = d;
+            d = temp;
+
+            std::cout<<"Values after the swap \n ";
+            std::cout<<"A = "<<a<<" B = "<<b<<" C = "<<c<<" D = "<<d;
+
+        }
+        break;
+        
+        case 5:
+        {
+            //supplement problem 2.1.4
+            
+            int a , b;
+            std::cout<<"enter value of A: ";
+            std::cin>>a;
+            std::cout<<"enter value of B: ";
+            std::cin>>b;
+            std::cout<<"Values before the swap \n ";
+            std::cout<<"A = "<<a<<" B = "<<b;
+
+
+            a = a+b;
+            b = a-b;
+            a = a-b;
+
+            std::cout<<"Values after the swap\n";
+            std::cout<<"A = "<<a<<" B = "<<b;
+
+        }
+        break;
+
+        case 6:
+        {
+            //supplement problem 2.1.5
+            int some_num[] = {1 ,3 ,5 ,7 ,9 ,11 , 13};
+            int i = 3;
+            std::cout<<std::endl;
+            for(int j =  0 ; j <7 ; j++ )
+            {
+                std::cout<<some_num[j]<<"\t";
+            }
+            std::cout<<std::endl;
+            std::cout<<"i = "<<i<<std::endl;
+            std::cout<<"Exchanging i with a[i]";
+
+            int temp;
+            temp = i;
+            i = some_num[i];
+            some_num[i] = temp ;
+
+            std::cout<<std::endl;
+            for(int j =  0 ; j <7 ; j++ )
+            {
+                std::cout<<some_num[j]<<"\t";
+            }
+            std::cout<<std::endl;
+            std::cout<<"i = "<<i<<std::endl;
+        }
+        break;
+
+        default:
+        std::cout<<"wrong choice "<<std::endl;
+        break;
+    }
+
 }
 void twopointtwo()
 {
@@ -54,6 +209,7 @@ void twopointnine()
 }
 void UI()
 {
+    int choice;
     std::cout<<"2.1 Exchanging the values"<<std::endl;
     
     std::cout<<"2.2 Counting"<<std::endl;
@@ -72,9 +228,11 @@ void UI()
     std::cout<<"2.8 Base conversion "<<std::endl;
     
     std::cout<<"2.9 Character to number conversion "<<std::endl;
+    std::cout<<"Enter your choice: 2.";
+    std::cin>>choice;
 
 
-    switch()
+    switch(choice)
     {
         case 1: twopointone();
         break;
