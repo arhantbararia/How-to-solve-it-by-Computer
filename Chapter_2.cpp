6df39cfab2494ae4b210be8d56ad2fbd08bbdee3
arhantbararia <arhantbararia@gmail.com>
2021-09-13 23:55:30 +0530

    UI Created

---
+++ Chapter_2.cpp
@@ -0,0 +1,119 @@
+#include<iostream>
+#include<stdlib.h>
+
+
+void twopointone()
+{
+    system("CLS");
+}
+void twopointtwo()
+{
+    system("CLS");
+    
+}
+
+void twopointthree()
+{
+    system("CLS");
+    
+}
+
+void twopointfour()
+{
+    system("CLS");
+    
+}
+
+void twopointfive()
+{
+    system("CLS");
+    
+}
+
+void twopointsix()
+{
+    system("CLS");
+
+}
+
+void twopointseven()
+{
+    system("CLS");
+
+}
+void twopointeight()
+{
+    system("CLS");
+
+}
+
+void twopointnine()
+{
+    system("CLS");
+
+}
+void UI()
+{
+    std::cout<<"2.1 Exchanging the values"<<std::endl;
+    
+    std::cout<<"2.2 Counting"<<std::endl;
+    
+
+    std::cout<<"2.3 Summation of set of numbers"<<std::endl;
+    
+    std::cout<<"2.4 Factorial Computation"<<std::endl;
+    
+    std::cout<<"2.5 Sine Function computation"<<std::endl;
+    
+    std::cout<<"2.6 Generating Fibbonacci Sequence"<<std::endl;
+    
+    std::cout<<"2.7 Reversing the digits of a number "<<std::endl;
+    
+    std::cout<<"2.8 Base conversion "<<std::endl;
+    
+    std::cout<<"2.9 Character to number conversion "<<std::endl;
+
+
+    switch()
+    {
+        case 1: twopointone();
+        break;
+
+        case 2: twopointtwo();
+        break;
+
+        case 3: twopointthree();
+        break;
+
+        case 4: twopointfour();
+        break;
+
+        case 5: twopointfive();
+        break;
+
+        case 6: twopointsix();
+        break;
+
+        case 7: twopointseven();
+        break;
+
+        case 8: twopointeight();
+        break;
+
+        case 9: twopointnine();
+        break;
+
+    }
+}
+
+
+int main()
+{
+    UI();
+    
+
+    
+    return 0;
+
+
+}
